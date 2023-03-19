91b7520c5e61d9fa3b726b14dd18f2b745d5026e
pavan28-28 <pavan2841999@gmail.com>
2023-03-19 12:59:42 +0530

    second commit

--- src/com/ecommerce/main/MainApplication.java
+++ src/com/ecommerce/main/MainApplication.java
@@ -2,6 +2,6 @@ package com.ecommerce.main;
 
 public class MainApplication {
 	public static void main(String args[]) {
-		System.out.println("inside1 ecommece appication");
+		System.out.println("inside1 ecommece appication......");
 	}
 }
